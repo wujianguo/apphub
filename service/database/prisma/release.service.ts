5c18e612a0f87b83403f8773f162205ccc0d216e
Jianguo Wu <lsjustin89@gmail.com>
2024-03-26 05:10:14 +0000

    feat: db2

--- service/database/prisma/release.service.ts
+++ service/database/prisma/release.service.ts
@@ -10,35 +10,39 @@ export class ReleaseService implements ReleaseInterface {
     private readonly app: AppModel) { }
 
   async createRelease(release: ReleaseCreateDto): Promise<ReleaseModel> {
-    const data = await prisma.release.create({
-      data: {
-        name: release.name,
-        fingerprint: release.fingerprint,
-        version: release.version,
-        shortVersion: release.shortVersion,
-        bundleIdentifier: release.bundleIdentifier,
-        description: release.description,
-        commitId: release.commitId,
-        minimumPlatformVersion: release.minimumPlatformVersion,
-        platformType: platformTypeToInt(release.platform),
-        releaseType: releaseTypeToInt(release.releaseType),
-        buildType: buildTypeToInt(release.buildType),
-        applicationId: parseInt(this.app.id),
-        file: {
-          create: {
-            name: release.file.name,
-            path: release.file.url,
-            size: release.file.size,
-          }
-        }
-      }
-    });
-    const model = new ReleaseModel();
-    model.fullfill(release);
-    model.id = data.id.toString();
-    model.createdAt = data.createdAt;
-    model.updatedAt = data.updatedAt;
-    return model;
+    throw new Error("Method not implemented.");
+    // const data = await prisma.release.create({
+    //   data: {
+    //     index: 1,
+    //     name: release.name,
+    //     fingerprint: release.fingerprint,
+    //     version: release.version,
+    //     shortVersion: release.shortVersion,
+    //     bundleIdentifier: release.bundleIdentifier,
+    //     description: release.description,
+    //     commitId: release.commitId,
+    //     minimumPlatformVersion: release.minimumPlatformVersion,
+    //     platformType: platformTypeToInt(release.platform),
+    //     releaseType: releaseTypeToInt(release.releaseType),
+    //     buildType: buildTypeToInt(release.buildType),
+    //     applicationId: parseInt(this.app.id),
+    //     file: {
+    //       create: {
+    //         name: release.file.name,
+    //         path: release.file.url,
+    //         size: release.file.size,
+    //       }
+    //     },
+    //     icon: undefined,
+    //     symbol: undefined,
+    //   }
+    // });
+    // const model = new ReleaseModel();
+    // model.fullfill(release);
+    // model.id = data.id.toString();
+    // model.createdAt = data.createdAt;
+    // model.updatedAt = data.updatedAt;
+    // return model;
   }
 
   getReleaseList(page = 1, perPage = 10, platform?: PlatformType): Promise<Pagination<ReleaseModel>> {
