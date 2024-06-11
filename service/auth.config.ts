60077ec29a7587fabc4c709a0151244550b45695
wujianguo <lsjustin89@gmail.com>
2024-06-11 13:53:25 +0800

    feat: allowDangerousEmailAccountLinking

--- service/auth.config.ts
+++ service/auth.config.ts
@@ -2,13 +2,8 @@ import type { NextAuthConfig } from "next-auth"
 import { Provider } from "next-auth/providers"
 import GitHub from "next-auth/providers/github"
 import GitLab from "next-auth/providers/gitlab"
-// import Resend from "next-auth/providers/resend"
- 
-// const providers: Provider[] = [Resend({
-//   from: process.env.AUTH_RESEND_FROM,
-// }), GitHub, GitLab]
 
-const providers: Provider[] = [GitHub, GitLab]
+const providers: Provider[] = [GitHub({ allowDangerousEmailAccountLinking: true }), GitLab({ allowDangerousEmailAccountLinking: true })]
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
@@ -16,6 +11,9 @@ export default {
   session: {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  pages: {
+    signIn: "/auth/login",
   },
   callbacks: {
     jwt({ token, user }) {
